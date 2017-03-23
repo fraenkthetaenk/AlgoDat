731e91ea62c97415116c675be65940ab07a7ec9d
frank <frank@Frank-Desktop.Speedport_W_724V_Typ_A_05011603_05_020>
2017-03-23 19:22:46 +0100

    Fix a bug with half sorted, added both sorted

--- src/uebung2/Main.java
+++ src/uebung2/Main.java
@@ -3,7 +3,11 @@ package uebung2;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println(SetIntersection.countIntersectionUnsorted(new int[]{2,9,5,6,17,11}, new int[] {17,1,100,5}));
+		
+		System.out.println(SetIntersection.countIntersectionOneInputOrdered(new int[]{2,9,6,5,1,11}, new int[]{1,5,6,12,13}));
+		
+		System.out.println(SetIntersection.countIntersectionOrdered(new int[] {1,2,5,6,9,11}, new int[]{1,5,6,12,13}));
 
 	}
 
--- src/uebung2/SetIntersection.java
+++ src/uebung2/SetIntersection.java
@@ -2,75 +2,81 @@ package uebung2;
 
 public class SetIntersection {
 
-	
-	
-	public static int countIntersectionUnsorted (int[] unsorted, int[] unsorted2 )
-	{
-		int countSame  =0;
-		for(int value1 : unsorted)
-		{
-			for (int value2 : unsorted2)
-			{
-				if(value1 == value2)
-				{
+	public static int countIntersectionUnsorted(int[] unsorted, int[] unsorted2) {
+		int countSame = 0;
+		for (int value1 : unsorted) {
+			for (int value2 : unsorted2) {
+				if (value1 == value2) {
 					countSame++;
 					continue;
-				}				
+				}
 			}
 		}
-		return countSame;		
+		return countSame;
 	}
-	
-	
-	public static int countIntersectionOneInputOrdered(int[] unsorted, int[] sorted)
-	{
-		int count= 0;
-		
-		for(int value1 : unsorted)
-		{			
-			if(sorted[0] == value1)
-			{
+
+	public static int countIntersectionOneInputOrdered(int[] unsorted, int[] sorted) {
+		int count = 0;
+
+		for (int value1 : unsorted) {
+			if (sorted[0] == value1) {
 				count++;
 				continue;
 			}
-			if(sorted[sorted.length-1] == value1)
-			{
+			if (sorted[sorted.length - 1] == value1) {
 				count++;
 				continue;
 			}
-			if(sorted[0] > value1)
-			{
+			if (sorted[0] > value1) {
 				continue;
 			}
-			if(sorted[sorted.length-1] < value1)
-			{
+			if (sorted[sorted.length - 1] < value1) {
 				continue;
 			}
-			
-			int lo =0 ;
-			int hi = sorted.length-1;
-			boolean found = false;			
-			while(lo<=hi && !found){
-				int middle = (lo+hi)/2;
-				if(sorted[middle]==value1)
-				{
-					count ++;
+
+			int lo = 0;
+			int hi = sorted.length - 1;
+			boolean found = false;
+			while (lo <= hi && !found) {
+				int middle = (lo + hi) / 2;
+				if (sorted[middle] == value1) {
+					count++;
 					found = true;
 					continue;
 				}
-				if(sorted[middle] > value1)
-				{
-					hi = middle +1;
+				if (sorted[middle] >value1) {
+					hi = middle - 1;
 					continue;
 				}
-				if(sorted[middle] < value1)
-				{
-					lo = middle +1;
+				if (sorted[middle] < value1) {
+					lo = middle + 1;
 					continue;
-				}				
-			}			
-		}		
+				}
+			}
+		}
 		return count;
-	}	
-	
+	}
+
+	public static int countIntersectionOrdered(int[] sorted, int[] sorted2) {
+		int count = 0;
+
+		int pos1 = 0;
+		int pos2 = 0;
+
+		while (pos1 < sorted.length && pos2 < sorted2.length) {
+			if (sorted[pos1] == sorted2[pos2]) {
+				count++;
+				pos1++;
+				pos2++;
+			} else if (sorted[pos1] > sorted2[pos2]) {
+				pos2++;
+
+			} else {
+				pos1++;
+			}
+		}
+
+		return count;
+
+	}
 }
