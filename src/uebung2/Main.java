72ec0632194757a9cf3626ecabfd4801fca5deda
Stefan <stefan.trageiser@gmx.de>
2017-03-28 16:35:31 +0200

    Teilaufgabe D eingebaut

--- src/uebung2/Main.java
+++ src/uebung2/Main.java
@@ -3,12 +3,43 @@ package uebung2;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println(SetIntersection.countIntersectionUnsorted(new int[]{2,9,5,6,17,11}, new int[] {17,1,100,5}));
-		
-		System.out.println(SetIntersection.countIntersectionOneInputOrdered(new int[]{2,9,6,5,1,11}, new int[]{1,5,6,12,13}));
-		
-		System.out.println(SetIntersection.countIntersectionOrdered(new int[] {1,2,5,6,9,11}, new int[]{1,5,6,12,13}));
+		System.out.println("Kontrolle der Ergebnisse:\n");
+		System.out.println("Teilaufgabe A: " + SetIntersection
+				.countIntersectionUnsorted(new int[] { 2, 9, 5, 6, 17, 11 }, new int[] { 17, 1, 100, 5 }));
 
+		System.out.println("Teilaufgabe B: " + SetIntersection
+				.countIntersectionOneInputOrdered(new int[] { 2, 9, 6, 5, 1, 11 }, new int[] { 1, 5, 6, 12, 13 }));
+
+		System.out.println("Teilaufgabe C: " + SetIntersection.countIntersectionOrdered(new int[] { 1, 2, 5, 6, 9, 11 },
+				new int[] { 1, 5, 6, 12, 13 }));
+
+		System.out.println("\nLaufzeiten:\n");
+		
+		System.out.println("\nTeilaufgabe A: 10.000:");		
+		SetIntersection.meassureRuntime(0,10000);		
+		System.out.println("\nTeilaufgabe A: 100.000:");		
+		SetIntersection.meassureRuntime(0,100000);
+		System.out.println("\nTeilaufgabe A: 1.000.000:");		
+		System.out.println("hochgerechnet eine Stunde.");
+		//SetIntersection.meassureRuntime(0,1000000);
+		
+		System.out.println("\nTeilaufgabe B: 10.000:");		
+		SetIntersection.meassureRuntime(1, 10000);		
+		System.out.println("\nTeilaufgabe B: 100.000:");		
+		SetIntersection.meassureRuntime(1, 100000);		
+		System.out.println("\nTeilaufgabe B: 1.000.000:");		
+		SetIntersection.meassureRuntime(1, 1000000);		
+		System.out.println("\nTeilaufgabe B: 10.000.000:");		
+		SetIntersection.meassureRuntime(1, 10000000);
+		
+		System.out.println("\nTeilaufgabe C: 10.000:");		
+		SetIntersection.meassureRuntime(2, 10000);
+		System.out.println("\nTeilaufgabe C: 100.000:");
+		SetIntersection.meassureRuntime(2, 100000);
+		System.out.println("\nTeilaufgabe C: 1.000.000:");
+		SetIntersection.meassureRuntime(2, 1000000);
+		System.out.println("\nTeilaufgabe C: 10.000.000:");
+		SetIntersection.meassureRuntime(2, 10000000);
 	}
 
 }
--- src/uebung2/SetIntersection.java
+++ src/uebung2/SetIntersection.java
@@ -1,5 +1,7 @@
 package uebung2;
 
+import java.util.Random;
+
 public class SetIntersection {
 
 	public static int countIntersectionUnsorted(int[] unsorted, int[] unsorted2) {
@@ -8,7 +10,6 @@ public class SetIntersection {
 			for (int value2 : unsorted2) {
 				if (value1 == value2) {
 					countSame++;
-					continue;
 				}
 			}
 		}
@@ -44,7 +45,7 @@ public class SetIntersection {
 					found = true;
 					continue;
 				}
-				if (sorted[middle] >value1) {
+				if (sorted[middle] > value1) {
 					hi = middle - 1;
 					continue;
 				}
@@ -78,5 +79,60 @@ public class SetIntersection {
 
 		return count;
 
+	}
+
+	public static int[] randomSorted(int size) {
+		Random r = new Random();
+		int[] result = new int[size];
+		int randomValue = 0;
+		for (int i = 0; i < size; i++) {
+			randomValue += 1 + r.nextInt(10);
+			result[i] = randomValue;
+		}
+		return result;
+	}
+
+	public static int[] randomUnsorted(int size) {
+		Random r = new Random();
+		int[] result = randomSorted(size);
+		for (int i = 0; i < size; i++) {
+			int randomIndex = i + r.nextInt(size - i);
+			int swap = result[i];
+			result[i] = result[randomIndex];
+			result[randomIndex] = swap;
+		}
+		return result;
+	}
+
+	public static void meassureRuntime(int teilaufgabe, int size) {
+
+		int[] unsorted = randomUnsorted(size);
+		int[] unsorted2 = randomUnsorted(size);
+		int[] unsorted3 = randomUnsorted(size);
+		int[] sorted = randomSorted(size);
+		int[] sorted2 = randomSorted(size);
+		int[] sorted3 = randomSorted(size);
+
+		if (teilaufgabe == 0) {
+			long start = System.currentTimeMillis();
+			countIntersectionUnsorted(unsorted, unsorted2);
+			long end = System.currentTimeMillis();
+			double runtime = (end - start) / 1000.0;
+			System.out.println("Laufzeit: " + runtime + "s.");
+		} else if (teilaufgabe == 1) {
+			long start = System.currentTimeMillis();
+			countIntersectionOneInputOrdered(unsorted3, sorted3);
+			long end = System.currentTimeMillis();
+			double runtime = (end - start) / 1000.0;
+			System.out.println("Laufzeit: " + runtime + "s.");
+		} else if (teilaufgabe == 2) {
+			long start = System.currentTimeMillis();
+			countIntersectionOrdered(sorted, sorted2);
+			long end = System.currentTimeMillis();
+			double runtime = (end - start) / 1000.0;
+			System.out.println("Laufzeit: " + runtime + "s.");
+		} else {
+			System.out.println("Bitte geben Sie eine gültige Teilaufgabe an!");
+		}
 	}
 }
